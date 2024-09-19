7daa0156e17c3d83f1af249f75a63cf51980063e
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2024-09-19 14:31:05 +0200

    20240919 a

--- src/main/java/de/kheuwes/footballforwall/model/Statuseintrag.java
+++ src/main/java/de/kheuwes/footballforwall/model/Statuseintrag.java
@@ -7,59 +7,90 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Statuseintrag {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public int dieminute = 0;
     public String typ = "";
-    public String name = "";
-    public String bemerkung = "";
-    
-    @Override
-    public String toString() {
-        return "Statuseintrag [id=" + id + ", minute=" + dieminute + ", typ=" + typ + ", name=" + name + ", bemerkung="
-                + bemerkung + "]";
-    }
+    public int spielminute = 0;
+    public String hg = "";
+    public int rueckennumer = 0;
+    public String spielername = "";
+    public String zusatz = "";
 
     public Statuseintrag() {
     }
 
-    public Statuseintrag(int minute, String typ, String name, String bemerkung) {
-        this.dieminute = minute;
+    public Statuseintrag(String typ, int spielminute, String hg, int rueckennumer, String spielername, String zusatz) {
         this.typ = typ;
-        this.name = name;
-        this.bemerkung = bemerkung;
+        this.spielminute = spielminute;
+        this.hg = hg;
+        this.rueckennumer = rueckennumer;
+        this.spielername = spielername;
+        this.zusatz = zusatz;
     }
+
+    @Override
+    public String toString() {
+        return "Statuseintrag [id=" + id + ", typ=" + typ + ", spielminute=" + spielminute + ", hg=" + hg
+                + ", rueckennumer=" + rueckennumer + ", spielername=" + spielername + ", zusatz=" + zusatz + "]";
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public int getDieminute() {
-        return dieminute;
-    }
-    public void setDieminute(int minute) {
-        this.dieminute = minute;
-    }
+
     public String getTyp() {
         return typ;
     }
+
     public void setTyp(String typ) {
         this.typ = typ;
     }
-    public String getName() {
-        return name;
+
+    public int getSpielminute() {
+        return spielminute;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setSpielminute(int spielminute) {
+        this.spielminute = spielminute;
     }
-    public String getBemerkung() {
-        return bemerkung;
+
+    public String getHg() {
+        return hg;
     }
-    public void setBemerkung(String bemerkung) {
-        this.bemerkung = bemerkung;
+
+    public void setHg(String hg) {
+        this.hg = hg;
     }
+
+    public int getRueckennumer() {
+        return rueckennumer;
+    }
+
+    public void setRueckennumer(int rueckennumer) {
+        this.rueckennumer = rueckennumer;
+    }
+
+    public String getSpielername() {
+        return spielername;
+    }
+
+    public void setSpielername(String spielername) {
+        this.spielername = spielername;
+    }
+
+    public String getZusatz() {
+        return zusatz;
+    }
+
+    public void setZusatz(String zusatz) {
+        this.zusatz = zusatz;
+    }
+
+    
 
 }
